c971b5b361bebc9e010b8ee1f97f9d209a78108a
Azeez <azeezabdullah7@gmail.com>
2022-04-22 12:42:10 +0100

    Test

--- main.h
+++ main.h
@@ -1,44 +1,15 @@
-#include "main.h"
+#ifndef PRINTF_H
+#define PRINTF_H
 
-char *_itoa(int num)
-{
-	char *str;
-	int i = 1, m, k = 0, len, power, keep;
-	len = num < 0 ? 3 : 2;
-	k = num < 0 ? 1 : 0;
-	num *= num < 0 ? -1 : 1;
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <limits.h>
 
-	keep = num;
+void _sprintf(const char *str);
+int _printf(const char *format, ...);
+void _count(int *ptr, char *str);
+char *_itoa(int num);
 
-	while (keep > 9)
-	{
-		keep /= 10;
-		len++;
-	}
-
-	power = len - 2;
-
-	str = malloc(sizeof(char) * len);
-	if (str == NULL)
-	{
-		exit (1);
-	}
-	else if (k == 1)
-	{
-		str[0] = '-';
-		power -= 1;
-	}
-
-	for (m = 0; m < power; m++)
-	{
-		i *= 10;
-	}
-	while (i >= 1)
-	{
-		str[k] = ((num / i) % 10) + '0';
-		i /= 10;
-		k++;
-	}
-	str[k] = '\0';
-	return (str);
-}
+#endif
